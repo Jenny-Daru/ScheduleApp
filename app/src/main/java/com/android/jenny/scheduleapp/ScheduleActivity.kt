0d4ddc578ffd86b9d163c7bff6e260d6ae391ea7
jhYun <jhyun@olivendove.com>
2021-09-13 18:03:06 +0900

    09.13 월 - UI Design

--- app/src/main/java/com/android/jenny/scheduleapp/ScheduleActivity.kt
+++ app/src/main/java/com/android/jenny/scheduleapp/ScheduleActivity.kt
@@ -1,11 +1,18 @@
 package com.android.jenny.scheduleapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_schedule_detail)
+        setContentView(R.layout.activity_schedule)
+    }
+
+    fun addTimeClick(view: View) {
+        var intent = Intent(this, ScheduleDetailActivity::class.java)
+        startActivity(intent)
     }
 }
